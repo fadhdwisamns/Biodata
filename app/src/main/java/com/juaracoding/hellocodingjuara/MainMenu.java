b9b6fb1de9922e85d98b52e571b471488478d325
dewabrata <dewabrata@gmail.com>
2020-07-22 11:29:51 +0700

    Update Screen TambahData

--- app/src/main/java/com/juaracoding/hellocodingjuara/MainMenu.java
+++ app/src/main/java/com/juaracoding/hellocodingjuara/MainMenu.java
@@ -2,21 +2,33 @@ package com.juaracoding.hellocodingjuara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
 
     String username, password;
     TextView txtUsername;
+    ImageView btnTambahData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         txtUsername = findViewById(R.id.txtUsername);
+        btnTambahData = findViewById(R.id.btnTambahData);
         username = getIntent().getStringExtra("username");
         password = getIntent().getStringExtra("password");
 
+
+
         txtUsername.setText(username);
+    }
+
+    public void screenTambahData(View view){
+        Intent intent = new Intent(MainMenu.this,TambahData.class);
+        startActivity(intent);
     }
 }
